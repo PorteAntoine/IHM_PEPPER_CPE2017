5bc2a807fffe48f322759975d61216ee4f8ae483
asaunier1 <34674666+asaunier1@users.noreply.github.com>
2018-01-09 16:53:54 +0100

    Add files via upload

--- Approche_dynamique/Object.py
+++ Approche_dynamique/Object.py
@@ -2,11 +2,11 @@ class Object():
 
     def __init__(self):
         self.name = ""
+        self.type = ""
         self.category = ""
+        self.localization = ""
+        self.room = ""
         self.color = ""
         self.shape = ""
         self.size = ""
         self.weight = ""
-        self.type = ""
-        self.localization = ""
-
